f9cf810ae8866bec95a2573741c084e36f3dde75
Andras Nemeth <guni82hu@gmail.com>
2017-10-04 01:48:53 +0200

    scope mixing test

--- app/src/main/java/hu/andras/daggersample/di/InteractorModule.java
+++ app/src/main/java/hu/andras/daggersample/di/InteractorModule.java
@@ -1,7 +1,12 @@
 package hu.andras.daggersample.di;
 
+import java.util.Date;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
+import hu.andras.daggersample.di.scopes.ActivityScope;
 import hu.andras.daggersample.di.scopes.IteractorScope;
 import hu.andras.daggersample.interactor.Feature1Interactor;
 import hu.andras.daggersample.interactor.Feature2Interactor;
@@ -24,5 +29,14 @@ public class InteractorModule {
     @Provides
     Feature2Interactor provideFeature2Interactor(NetworkApi1 networkApi1) {
         return new Feature2Interactor(networkApi1);
+    }
+
+    /**
+     * You can use any scope, but you can reference it only from a matching component.
+     */
+    @ActivityScope
+    @Provides
+    Date randomObject() {
+        return new Date();
     }
 }
